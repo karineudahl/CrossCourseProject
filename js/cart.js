3d8bfad7f4c02acb50c205c34605b966eb94f646
karineudahl <karineudahl@hotmail.com>
2021-05-25 21:08:08 +0200

    update

---
+++ js/cart.js
@@ -0,0 +1,42 @@
+let addToCarts = document.querySelectorAll(".add-to-cart");
+const cartCount = document.querySelector(".cart-count");
+
+let products = [
+    {
+        name: "Lofoten Jacket",
+        tag: "lofotenjacket",
+        price: 1499,
+        inCart: 0
+    }
+]
+
+for (let i = 0; i < addToCarts.length; i++) {
+    addToCarts[i].addEventListener("click", () => {
+        cartNumbers()
+    })
+}
+
+function cartCountCheck() {
+    let productNumbers = localStorage.getItem("cartNumbers");
+
+    if(productNumbers) {
+        cartCount.innerHTML = productNumbers;
+    }
+}
+
+function cartNumbers() {
+    let productNumbers = localStorage.getItem("cartNumbers");
+
+    productNumbers = parseInt(productNumbers);
+
+    if(productNumbers) {
+        localStorage.setItem("cartNumbers", productNumbers + 1);
+        cartCount.innerHTML = `${productNumbers++}`;
+       
+    } else {
+        localStorage.setItem("cartNumbers", 1);
+        cartCount.innerHTML = `1`;
+    }
+}
+
+cartCountCheck();
